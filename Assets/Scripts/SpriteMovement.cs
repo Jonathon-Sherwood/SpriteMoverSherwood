80d02fd33e2c06b99ef67b19473d4f94d855df20
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2020-05-20 12:11:27 -0700

    Initial Sprite Movement
    Sprite automatically moves to the right at an adjustable rate.

---
+++ Assets/Scripts/SpriteMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMovement : MonoBehaviour
+{
+    //allows the designer to adjust the speed of the gameobjects movement speed in the inspector
+    public float movementSpeed = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //multiplies the movementspeed variable to the right vector on the gameobject by time rather than by framerate
+        transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+    }
+}
